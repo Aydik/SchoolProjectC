774222f7e92c768f9b25f4abff90bca1ebabb8a6
Aydik <nekkisuper@gmail.com>
2021-11-12 19:32:52 +0400

    С разделением на файлы

--- SchoolProjectC/main.py
+++ SchoolProjectC/main.py
@@ -1,9 +1,10 @@
 import sys
 from random import shuffle
 from PyQt5 import uic, QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog, QDialog, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog, QDialog
 from PyQt5.QtGui import QIcon
 import sqlite3
+from admin_window import AdminWindow
 
 
 class MyWidget(QMainWindow):
@@ -279,35 +280,8 @@ class MyWidget(QMainWindow):
         db.close()
 
     def show_db(self):  # открыть окно с вожмозностью просмотра бд
-        self.adminWindow = QDialog()
-        self.adminWindow.setFixedSize(800, 600)
-        self.adminWindow.setFont(QtGui.QFont("MS Shell Dlg 2", 14))
-        self.adminWindow.setWindowIcon(QIcon('Images/favicon.ico'))
-        self.adminWindow.setWindowTitle('Решения')
-        self.tableWidget = QtWidgets.QTableWidget(self.adminWindow)
-        self.tableWidget.resize(780, 500)
-        self.tableWidget.move(10, 10)
-        self.button_update = QtWidgets.QPushButton(self.adminWindow)
-        self.button_update.resize(780, 60)
-        self.button_update.move(10, 520)
-        self.button_update.setText("Обновить")
-        self.button_update.clicked.connect(self.update_db)
-        self.update_db()
-        self.adminWindow.show()
-
-    def update_db(self):  # обновить окно с  просмотра бд
-        db = sqlite3.connect("decision.db")
-        cur = db.cursor()
-        result = cur.execute("""SELECT * FROM Students""").fetchall()
-        if result:
-            self.tableWidget.setRowCount(len(result))
-            self.tableWidget.setColumnCount(len(result[0]))
-            for i, elem in enumerate(result):
-                for j, val in enumerate(elem):
-                    self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
-        self.tableWidget.setHorizontalHeaderLabels(
-            ("Имя", "Задание 1", "Задание 2", "Задание 3", "Задание 4", "Итого"))
-        db.close()
+        self.cams = AdminWindow()
+        self.cams.show()
 
 
 if __name__ == '__main__':
